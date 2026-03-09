d8a8c45727d3efc540045d0826c6d283318ef98c
kris <cyberoto79@gmail.com>
2026-03-09 13:22:30 +0900

    Fix: AssemblyInfo.cs 버전 2.4.3.0으로 업데이트

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -12,9 +12,9 @@ using System.Windows;
 [assembly: AssemblyCulture("")]
 
 // Version information
-[assembly: AssemblyVersion("2.4.1")]
-[assembly: AssemblyFileVersion("2.4.1")]
-[assembly: AssemblyInformationalVersion("2.4.1")]
+[assembly: AssemblyVersion("2.4.3.0")]
+[assembly: AssemblyFileVersion("2.4.3.0")]
+[assembly: AssemblyInformationalVersion("2.4.3")]
 
 [assembly: ThemeInfo(
     ResourceDictionaryLocation.None,            //where theme specific resource dictionaries are located
